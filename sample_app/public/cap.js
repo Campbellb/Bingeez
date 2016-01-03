2a07157d3fe65f2ba83a93def3881a206b27c80a
Nika Jones <github@nikajon.es>
2016-01-03 17:10:11 +0000

    The one with the call back.

--- sample_app/public/cap.js
+++ sample_app/public/cap.js
@@ -2,15 +2,30 @@
 var socket = io.connect();
 Date.ts = function() { return Math.ceil(new Date().getTime() / 1000); }
 
-function init() {
+var intervals = []
+
+function startItBackUp(session, channel) {
+    socket.emit('log', {"session":session, "show":channel });
+    socket.emit('back_pay', {"session":session, "show":channel });
+}
+
+function startItUp(session, channel) {
+    if (intervals.length > 0 ) {
+        for (i in intervals) {
+            var ci = intervals.pop()
+            clearInterval(ci)
+
+        }
+    }
+
   // we will fake sending live data every two seconds.
-  setInterval(send_live_data, 2000);
+  intervals.push(setInterval(send_live_data, 2000));
   socket.on('amount', function(data) {
     cc = document.getElementById("bingeez-earned");
     cc.textContent = data["currency_a"];
   });
-}
 
-function send_live_data() {
-    socket.emit('collect_pay', {"session":"aa", "show":"apple tv", "timestamp": Date.ts() });
+  function send_live_data() {
+      socket.emit('collect_pay', {"session":session, "show":channel, "timestamp": Date.ts() });
+  }
 }
